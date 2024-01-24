d36f3da4afdfc34910f8337995756702c90af3a1
Martin Fernandez <Electronicsmf@aol.com>
2024-01-23 20:10:29 -0500

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly7.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}
