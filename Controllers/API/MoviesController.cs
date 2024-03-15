b53d153011fd0fd8b5421ffb71b123b7f5cb9217
Martin Fernandez <Electronicsmf@aol.com>
2024-03-15 07:12:39 -0400

    creating and API

---
+++ Controllers/API/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vidly7.Data;
+
+namespace Vidly7.Controllers.API
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MoviesController : ControllerBase
+	{
+		private readonly ApplicationDbContext _context;
+
+		public MoviesController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		//GET /api/movies
+		[HttpGet]
+		[Route("/api/movies")]
+		public IActionResult GetMovies()
+		{
+			var movies = _context.Movies.
+				 Include(m => m.Genre)
+				 .ToList();
+			return Ok(movies);
+		}
+
+
+
+	}
+}
